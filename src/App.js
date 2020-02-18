14c09e846a83c760efb681e4a0c0a6c970095204
Marius Rhein <marius.rhein95@gmail.com>
2020-02-18 17:19:57 +0100

    Use react boilerplate

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div class="container">
+        <div class="header">Header</div>
+        <div class="context">context</div>
+        <div class="content">Content</div>
+        <div class="footer">Footer</div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
