8b4f69495efe0dcea66b1721685c041e2e8f9986
Marius Rhein <marius.rhein95@gmail.com>
2020-02-19 01:03:52 +0100

    Fix centering of blog entries

--- src/components/overview.js
+++ src/components/overview.js
@@ -7,7 +7,7 @@ function Overview(props) {
   return (
     <div className="entryContainer">
       {props.blogEntries.map(blogEntry => (
-        <div key={blogEntry.id}>
+        <div style={{ width: "230px", margin: "auto" }} key={blogEntry.id}>
           <RandomImage></RandomImage>
           <NavLink className="navLink" to={`/blogEntry/${blogEntry.id}`}>
             <div className="overviewEntry">{blogEntry.title}</div>
