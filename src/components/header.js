05b2738789d3bd44ea692407a00ba56f8a5f852a
Marius Rhein <marius.rhein95@gmail.com>
2020-02-19 15:02:27 +0100

    Update header apperance

--- src/components/header.js
+++ src/components/header.js
@@ -6,62 +6,64 @@ import "../css/header.css";
 
 function Header(props) {
   return (
-    <div className="header" id="blogHeader">
-      <NavLink to="/">
-        <FontAwesomeIcon icon={faBlog} />
-      </NavLink>
+    <div className="wrapper">
+      <div className="header" id="blogHeader">
+        <NavLink to="/">
+          <FontAwesomeIcon icon={faBlog} />
+        </NavLink>
 
-      <div className="dropdown">
-        <button className="dropbtn">Item 1</button>
-        <div className="dropdown-content">
-          <a href="#">Link 1</a>
-          <a href="#">Link 2</a>
-          <a href="#">Link 3</a>
+        <div className="dropdown">
+          <button className="dropbtn">Item 1</button>
+          <div className="dropdown-content">
+            <a href="#">Link 1</a>
+            <a href="#">Link 2</a>
+            <a href="#">Link 3</a>
+          </div>
         </div>
-      </div>
-      <div className="dropdown">
-        <button className="dropbtn">Item 2</button>
-        <div className="dropdown-content">
-          <a href="#">Link 1</a>
-          <a href="#">Link 2</a>
-          <a href="#">Link 3</a>
+        <div className="dropdown">
+          <button className="dropbtn">Item 2</button>
+          <div className="dropdown-content">
+            <a href="#">Link 1</a>
+            <a href="#">Link 2</a>
+            <a href="#">Link 3</a>
+          </div>
         </div>
-      </div>
-      <div className="dropdown">
-        <button className="dropbtn">Item 3</button>
-        <div className="dropdown-content">
-          <a href="#">Link 1</a>
-          <a href="#">Link 2</a>
-          <a href="#">Link 3</a>
+        <div className="dropdown">
+          <button className="dropbtn">Item 3</button>
+          <div className="dropdown-content">
+            <a href="#">Link 1</a>
+            <a href="#">Link 2</a>
+            <a href="#">Link 3</a>
+          </div>
         </div>
-      </div>
-      <div className="dropdown">
-        <button className="dropbtn">Item 4</button>
-        <div className="dropdown-content">
-          <a href="#">Link 1</a>
-          <a href="#">Link 2</a>
-          <a href="#">Link 3</a>
+        <div className="dropdown">
+          <button className="dropbtn">Item 4</button>
+          <div className="dropdown-content">
+            <a href="#">Link 1</a>
+            <a href="#">Link 2</a>
+            <a href="#">Link 3</a>
+          </div>
         </div>
-      </div>
-      <div className="dropdown">
-        <button className="dropbtn">Item 5</button>
-        <div className="dropdown-content">
-          <a href="#">Link 1</a>
-          <a href="#">Link 2</a>
-          <a href="#">Link 3</a>
+        <div className="dropdown">
+          <button className="dropbtn">Item 5</button>
+          <div className="dropdown-content">
+            <a href="#">Link 1</a>
+            <a href="#">Link 2</a>
+            <a href="#">Link 3</a>
+          </div>
         </div>
-      </div>
-      <div className="dropdown">
-        <button className="dropbtn">Item 6</button>
-        <div className="dropdown-content">
-          <a href="#">Link 1</a>
-          <a href="#">Link 2</a>
-          <a href="#">Link 3</a>
+        <div className="dropdown">
+          <button className="dropbtn">Item 6</button>
+          <div className="dropdown-content">
+            <a href="#">Link 1</a>
+            <a href="#">Link 2</a>
+            <a href="#">Link 3</a>
+          </div>
         </div>
+        <a className="icon" onClick={clickMenuItem}>
+          <FontAwesomeIcon icon={faBars} />
+        </a>
       </div>
-      <a className="icon" onClick={clickMenuItem}>
-        <FontAwesomeIcon icon={faBars} />
-      </a>
     </div>
   );
 }
